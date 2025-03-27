0a429d4d79c28e1d31bf17870146de7f0682dff5
Alex <anadjarov@gmail.com>
2025-03-27 13:56:58 +0100

    Update PreviewViewController.swift

--- QLFAUSTExtension/PreviewViewController.swift
+++ QLFAUSTExtension/PreviewViewController.swift
@@ -9,21 +9,30 @@ class PreviewViewController: NSViewController, QLPreviewingController {
         view = NSView()
     }
 
+    var gestureRecognizer: NSClickGestureRecognizer?
+
     func preparePreviewOfFile(at url: URL) async throws {
         let swiftUIView = QLFAUSTView(fileURL: url)
             .frame(minWidth: 100, idealWidth: .infinity, maxWidth: .infinity)
             .frame(minHeight: 100, idealHeight: .infinity, maxHeight: .infinity)
+            .background(Color(NSColor.windowBackgroundColor))
 
         let hostingView = NSHostingView(rootView: swiftUIView)
         hostingView.frame = CGRect(x: 0, y: 0, width: 800, height: 600)
 
-        // Optionally: Resize the parent view to match the hosting view size
         view.setFrameSize(hostingView.frame.size)
+        view.wantsLayer = false
 
-        view.addSubview(hostingView)
+        view.addSubview(hostingView, positioned: .above, relativeTo: nil)
 
         hostingView.frame = view.bounds
         hostingView.autoresizingMask = [.width, .height]
         hostingView.frame.origin = CGPoint(x: 0, y: 0)
+
+        hostingView.wantsLayer = true
+        hostingView.layerContentsRedrawPolicy = .onSetNeedsDisplay
+        hostingView.setAccessibilityRole(.group)
+
+        view.window?.makeFirstResponder(hostingView)
     }
 }
