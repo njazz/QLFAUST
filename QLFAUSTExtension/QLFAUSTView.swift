76b4484f02d39b69fdf0ee4a065edf67838c673e
Alex <anadjarov@gmail.com>
2025-03-27 01:01:55 +0100

    Update QLFAUSTView.swift

--- QLFAUSTExtension/QLFAUSTView.swift
+++ QLFAUSTExtension/QLFAUSTView.swift
@@ -12,9 +12,11 @@ import FaustSwiftUI
 // MARK: -
 
 class FaustUIProxy: FaustUIValueBinding {
-    var faustDspObject: FaustDSPObject?
+    @Published public var faustDspObject: FaustDSPObject?
+    @Published public  var values: [String: Double] = [:]   // rewuired for UI updates
 
     public func getValue(for address: String, default defaultValue: Double) -> Double {
+        
         if let dsp = faustDspObject {
             return Double(dsp.getParameter(address))
         }
@@ -22,8 +24,9 @@ class FaustUIProxy: FaustUIValueBinding {
     }
 
     public func setValue(_ value: Double, for address: String) {
+        values[address]  = value
         if let dsp = faustDspObject {
-            dsp.setParameter(address, value: Float(value))
+             dsp.setParameter(address, value: Float(value))
         }
     }
 }
@@ -32,8 +35,6 @@ class FaustUIProxy: FaustUIValueBinding {
 
 struct QLFAUSTView: View {
     let fileURL: URL
-
-    @State private var selectedTab = 0
 
     @State private var faustString = ""
     @State private var jsonString = "[]"
@@ -45,15 +46,13 @@ struct QLFAUSTView: View {
 
     @StateObject private var audioEngine: AudioEngine
 
-    public init(fileURL: URL, selectedTab: Int = 0) {
+    public init(fileURL: URL) {
         self.fileURL = fileURL
-        self.selectedTab = selectedTab
-
-        uiProxy = FaustUIProxy()
+       uiProxy = FaustUIProxy()
 
         let file = fileURL.absoluteString.replacingOccurrences(of: "file://", with: "")
 
-        var errorString: NSString?
+        var errorString: NSString? = NSString()
 
         if let result = FaustDSPObject(path: file, error: &errorString) {
             _jsonString = State(initialValue: result.getUIJSON())
@@ -62,11 +61,10 @@ struct QLFAUSTView: View {
             _error = State(initialValue: "")
             _audioEngine = StateObject(wrappedValue: AudioEngine())
 
-            uiProxy.faustDspObject = faustDspObject
+             uiProxy.faustDspObject = faustDspObject
 
         } else {
-            _jsonString = State(initialValue: "[]")
-            _faustString = State(initialValue: "")
+            _jsonString = State(initialValue: "{\"ui\":[]}")
             _svgString = State(initialValue: nil)
             _faustDspObject = State(initialValue: nil)
             _audioEngine = StateObject(wrappedValue: AudioEngine())
@@ -88,115 +86,137 @@ struct QLFAUSTView: View {
 
     @ViewBuilder
     private var render: some View {
-        // toolbar:
-        HStack {
-            Button(action: {
-                displayCode.toggle()
-            }) {
-                Image(systemName: "doc.plaintext")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 24, height: 24)
-                    .padding()
-                    .foregroundColor(displayCode ? .blue : .gray)
-            }
-            .buttonStyle(.plain)
+        VStack(alignment: .leading) {
             
-            Button(action: {
-                displaySVG.toggle()
-            }) {
-                Image(systemName: "photo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 24, height: 24)
-                    .padding()
-                    .foregroundColor(displaySVG ? .blue : .gray)
-            }
-            .buttonStyle(.plain)
-            
-            Button(action: {
-                displayDSP.toggle()
-            }) {
-                Image(systemName: "hifispeaker")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 24, height: 24)
-                    .padding()
-                    .foregroundColor(displayDSP ? .blue : .gray)
-            }
-            .buttonStyle(.plain)
-            
-            HStack {
-                if audioEngine.available {
-                    let engine = audioEngine
-
-                    Button(action: {
-                        engine.isRunning ? engine.stop() : engine.start()
-                    }) {
-//                        Text(engine.isRunning ? "Stop Audio" : "Start Audio")
-//                            .padding()
-//                            .frame(maxWidth: .infinity)
-//                            .frame(height: 30)
-//                            .background(engine.isRunning ? Color.red : Color.green)
-//                            .foregroundColor(.white)
-//                            .animation(.easeInOut(duration: 0.2), value: engine.isRunning)
-                        
-                        Image(systemName: engine.isRunning ? "stop.circle.fill" : "play.circle.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 24, height: 24)
-                            .padding()
-                            .foregroundColor(engine.isRunning ? .gray : .blue)
-                    }
-                    .frame(width: 120, height: 30)
-                    .buttonStyle(.plain)
-                    
-                    Text(engine.statusMessage)
-                    
-                    Text("Inputs: \(_faustDspObject.wrappedValue!.getInputsCount())").frame(maxWidth:80)
-                    Text("Outputs: \(_faustDspObject.wrappedValue!.getOutputsCount())").frame(maxWidth:80)
-                    
-
-                } else {
-                    Text(audioEngine.statusMessage)
-                        .foregroundColor(.red)
+            // toolbar:
+            HStack() {
+                Button(action: {
+                    displayCode.toggle()
+                }) {
+                    Image(systemName: "doc.plaintext")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
                         .padding()
+                        .foregroundColor(displayCode ? .blue : .gray)
                 }
-            }
-            .frame(height: 32)
-            .cornerRadius(5)
-        }
-        .padding()
-        .frame(height: 32)
-        .cornerRadius(5)
-
-        HStack {
-            if displayCode || (!displaySVG && !displayDSP) {
-                QLFAUST_UI.PreviewFaustSource(faustString: faustString)
-            }
-
-            VStack {
-                if displaySVG {
-                    if let svgString = svgString {
-                        QLFAUST_UI.PreviewSVG(svgString: svgString)
-
-                    } else {
-                        Text("No SVG available")
+                .buttonStyle(.plain)
+                
+                Button(action: {
+                    displaySVG.toggle()
+                }) {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+                        .padding()
+                        .foregroundColor(displaySVG ? .blue : .gray)
+                }
+                .buttonStyle(.plain)
+                
+                Button(action: {
+                    displayDSP.toggle()
+                }) {
+                    Image(systemName: "hifispeaker")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+                        .padding()
+                        .foregroundColor(displayDSP ? .blue : .gray)
+                }
+                .buttonStyle(.plain)
+                
+                HStack {
+                    if audioEngine.available {
+                        let engine = audioEngine
+                        
+                        Button(action: {
+                            engine.isRunning ? engine.stop() : engine.start()
+                        }) {
+                            
+                            Image(systemName: engine.isRunning ? "stop.circle.fill" : "play.circle.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 24, height: 24)
+                                .padding()
+                                .foregroundColor(engine.isRunning ? .gray : .blue)
+                        }
+                        .frame(width: 120, height: 30)
+                        .buttonStyle(.plain)
+                        
+                        Text(engine.statusMessage)
+                        
+                        Text("Inputs: \(_faustDspObject.wrappedValue!.getInputsCount())").frame(maxWidth:80)
+                        Text("Outputs: \(_faustDspObject.wrappedValue!.getOutputsCount())").frame(maxWidth:80)
+                        
+                        
                     }
-                }
-
-                // from here
-                if displayDSP {
-                    VStack {
-                        ScrollView {
-                            QLFAUST_UI.PreviewDSP(jsonString: jsonString, viewModel: uiProxy)
-                                .onAppear {
-                                    if let object = faustDspObject {
-                                        _audioEngine.wrappedValue.setup(object)
-                                    }
-                                }
+                    else {
+                        HStack(alignment: .center) {
+                            Text("Audio Engine Error: ")
+                                .multilineTextAlignment(.center)
+                            Text(_error.wrappedValue)
+                                .foregroundColor(.red)
                                 .padding()
                         }
+                    }
+                }
+                .frame(height: 32)
+                .cornerRadius(5)
+                
+            }
+
+            .frame(height: 32)
+            .cornerRadius(5)
+            
+            HStack {
+                if displayCode || (!displaySVG && !displayDSP) {
+                    QLFAUST_UI.PreviewFaustSource(faustString: faustString)
+                        .transition(.slide)
+                }
+                    
+                
+                VStack {
+                    if displaySVG {
+                        VStack {
+                            if let svgString = svgString {
+                                QLFAUST_UI.PreviewSVG(svgString: svgString)
+                                    .transition(.opacity)
+                                
+                            } else {
+                                VStack(alignment:.center){
+                                    Text("No SVG available").padding()
+                                }
+                            }
+                        }
+                        .transition(.slide)
+                    }
+                    
+                    // from here
+                    if displayDSP {
+                        VStack {
+                            if (faustDspObject != nil) {
+                                    ScrollView([.horizontal, .vertical]) {
+                                        QLFAUST_UI.PreviewDSP(jsonString: jsonString, viewModel: uiProxy)
+                                            .onAppear {
+                                                if let object = faustDspObject {
+                                                    _audioEngine.wrappedValue.setup(object)
+                                                }
+                                            }
+                                    }
+                                    //
+                                    .opacity(displayDSP ? 1 : 0)
+                                    .scaleEffect(displayDSP ? 1 : 0.25)
+                                    .animation(.easeInOut(duration: 0.25), value: displayDSP)
+                                
+                            }
+                            else {
+                                VStack(alignment:.center){
+                                    Text(_error.wrappedValue).padding()
+                                }
+                            }
+                        }
+                        .transition(.slide)
                     }
                 }
             }
